d48c68017ff43006e9cf4744534ac39dfc1404ad
Luciducidu <luca0820@gmx.de>
2026-03-19 17:18:36 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,9 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Professor-Nuzlock/',
   plugins: [
+    
     react(),
     VitePWA({
       registerType: 'autoUpdate',
